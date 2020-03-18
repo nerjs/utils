a35604d838439d366963f4407095805c8c1b6fe5
nerjs <alex.galmanov@gmail.com>
2020-03-18 23:33:18 +0200

    add HttpError class

---
+++ errors/HttpError.js
@@ -0,0 +1,25 @@
+const STATUS_CODES = require('./lib/statusCodes')
+
+
+
+class HttpError extends Error {
+
+    /**
+     * @class
+     * @name HttpError
+     * @param {Number} code http status code
+     * @param {String} message custom message
+     */
+    constructor(code, message) {
+        const mess = message || HttpError.STATUS_CODES[code || 500]
+        super(mess)
+
+        this.message = mess
+        this.code = code
+        this.name = 'HttpError'
+    }
+
+    static STATUS_CODES = STATUS_CODES
+}
+
+module.exports = HttpError
